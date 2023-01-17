5bb53506084c08b4c9ec7d0c63c775a01c7dd098
sberdichevskaya <122852888+sberdichevskaya@users.noreply.github.com>
2023-01-17 16:59:08 +0700

    Задание 3

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
-let mas = [6,1,7,3,5,8,0,-1,3,2,4,5];
 
-function binSearch(mas, item)
+let mas = [6,1,7,3,5,8,0,-1,3,2,4,5];
+let item = 0;
+
+function selectionSort(mas)
+{
+    for (let i = 0; i < mas.length; i++)
+    {
+        let indexMin = i;
+        for (let j = i+1; j < mas.length; j++)
+        {
+            if(mas[j] < mas[indexMin])
+            {
+                indexMin = j;
+            }
+            //console.log('цикл 2')
+        }
+        let temp = mas[i];
+        mas[i] = mas[indexMin];
+        mas[indexMin] = temp;
+        //console.log('цикл 1')
+        console.log(mas)
+    }
+    //console.log('вывод 1')
+    return mas;
+}
+
+let masSort = selectionSort(mas);
+console.log(selectionSort(masSort));
+
+function binSearch(masSort, item)
 {
     let start = 0;
-    let finish = mas.lenght;
+    let finish = mas.length;
     let middle;
     let found = false;
     let position = -1;
@@ -26,7 +54,8 @@ function binSearch(mas, item)
             start = middle + 1;
         }
     }
+    //console.log('вывод 2')
     return position;   
 }
 
-console.log(binSearch(mas, 0));
+console.log(binSearch(masSort, item));
